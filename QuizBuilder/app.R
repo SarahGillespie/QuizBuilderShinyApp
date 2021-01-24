e1b4fd3e3924f711c94b0acc6960f4588d334fdd
Sarah Gillespie <you@example.com>
2021-01-23 22:56:47 -0500

    added dropdown options and more comments
    ready for J to work on it

--- QuizBuilder/app.R
+++ QuizBuilder/app.R
@@ -38,11 +38,24 @@ ui <- fluidPage(
                         min = 1,
                         max = 50,
                         value = 30)
-        ),
+        ), # end sidebar panel
 
-        # Show a plot of the generated distribution
+        # Show a plot of the generated histogram distribution
         mainPanel(
-           plotOutput("distPlot"),
+           plotOutput("distPlot"), # SG: delete later
+           
+           # SG: prints a dropdown menu for a pre-loaded cars dataset.
+           # SG: TASK: swap the preloaded variables/states to be the quiz options for our app
+           # SG: docs all about the dropdown menu: https://shiny.rstudio.com/reference/shiny/latest/selectInput.html
+           # SG: this is only part 1 of the dropdown menu, UI part.
+           #     the part that then responds to the user selection is in the server.
+
+           selectInput("state", "Choose a state:",
+                       list(`East Coast` = list("NY", "NJ", "CT"),
+                            `West Coast` = list("WA", "OR", "CA"),
+                            `Midwest` = list("MN", "WI", "IA"))
+           ),
+           textOutput("result"),
            
            # SG: our text should be displayed below the histogram.
            mainPanel(
@@ -52,6 +65,7 @@ ui <- fluidPage(
                # SG: TASK: randomize the question order
                # SG: TASK: add the score counter.
                
+               # prints a question
                math_questions_df$V1[3],# SG: calls the cell that is in the 3rd row of the first column in the math df
                # SG: gotta figure out how to do a new line
                math_questions_df$V2[3],
@@ -60,16 +74,18 @@ ui <- fluidPage(
                math_questions_df$V5[3],
                math_questions_df$V6[3]
                
-               ) 
+               )
            
            
-        )
+        ) # end main panel
     )
 )# end of UI
 
-# SG: define functions here, but call them in server.
+# SG: we can define functions here (outside of both UI and server),
+#     but call them in server.
 
-# Define server logic required to draw a histogram
+# SG: the server is like the calculator part that tells the app how to respond
+#     to user imput.
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
@@ -81,7 +97,14 @@ server <- function(input, output) {
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
     
-    # SG: here we will bring in each question and its answer choices.
+    # SG: here is part 2 of the select input option, the server part. 
+    # SG: TASK: make the app print words stating the specific test that was chosen.
+    
+    output$result <- renderText({
+        paste("You chose", input$state)
+    })
+    
+    # SG: here we will bring in the user's answer choice response action.
     # SG: the user clicks a letter or we have a text box to type the answer.
     # SG: check the user input against the answer key.
     
